eea84c9a5a95542429143a4d41d4293c408b799f
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-11-15 04:29:00 -0800

    Prompt

--- simple_shell.c
+++ simple_shell.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
 /**
-  *
-  *
-  */
-
-void
+* main - prints a prompt for user
+* @ac: argument count
+* @av: argument vector
+* Return: 0
+*/
+int main(int ac, char **av)
+{
+	printf("apshell$ ");
+	return (0);
+}
