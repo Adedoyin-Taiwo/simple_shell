45fb09cc4ea902cea20c0c63cdf88a8e18f708a1
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-11-15 06:22:25 -0800

    CMA

--- main.h
+++ main.h
@@ -1,10 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define MAX_COMMAND_LENGTH 100
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 
 void _printf(const char *text);
 #endif
--- simple_shell.c
+++ simple_shell.c
@@ -6,8 +6,55 @@
 * @av: argument vector
 * Return: 0
 */
-int main(int ac, char **av)
+int main()
 {
+	char command[MAX_COMMAND_LENGTH];
+	size_t len;
+	pid_t pid;
+	int val;
+
 	printf("apshell$ ");
-	return (0);
+	fflush(stdout);
+	if (fgets(command, sizeof(command), stdin) == NULL)
+	{
+		printf("\nExiting shell...\n");
+		return (1);
+	}
+        len = strlen(command);
+        if (len > 0 && command[len - 1] == '\n')
+	{
+		command[len - 1] = '\0';
+        }
+	pid = fork();
+        if (pid == -1)
+	{
+		perror("Fork failed");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		char *args[MAX_COMMAND_LENGTH];
+                args[0] = command;
+                args[1] = NULL;
+		val = execvp(command, args);
+		if (val == -1)
+		{
+			perror("Error executing command");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			int status;
+			waitpid(pid, &status, 0);
+			if (WIFEXITED(status))
+			{
+				printf("Command executed successfully with exit status: %d\n", WEXITSTATUS(status));
+			}
+			else
+			{
+				printf("Command execution failed\n");
+			}
+		}
+	}
+	return 0;
 }
