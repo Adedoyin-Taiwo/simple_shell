e2ef18bc7f3fac8d78a1fb59b839fa9178de51f4
0xPrecious <113894926+Layna934@users.noreply.github.com>
2023-11-15 13:16:05 +0100

    Update _printf.c to fix Betty

--- _printf.c
+++ _printf.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include <unistd.h>
 
+/**
+ *_printf - function to write to standard output
+ *@text: the text to be written
+ *Return: nothing
+*/
+
 void _printf(const char *text)
 {
 	write(STDOUT_FILENO, text, strlen(text));
