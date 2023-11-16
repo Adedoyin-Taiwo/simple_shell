9b06cce606263cd60c0b070a895ab24bb16b093a
Adedoyin-Taiwo <adedoyinhephzeebaracademian@gmail.com>
2023-11-15 20:49:12 -0800

    close

--- simple_shell.c
+++ simple_shell.c
@@ -1,5 +1,99 @@
 #include "main.h"
 #include <unistd.h>
+
+/**
+* executeCommand - executes command
+* @command: the command
+* Return: nothing
+*/
+
+void executeCommand(const char *command)
+{
+	pid_t pid;
+	int val;
+	
+	char *non_const_command = strdup(command);
+	if (non_const_command == NULL)
+	{
+		perror("Memory allocation error");
+	}
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Fork failed");
+		return;
+	}
+	else if (pid == 0)
+	{
+		char *args[MAX_COMMAND_LENGTH];
+		args[0] = non_const_command;
+		args[1] = NULL;
+		val = execve(command, args, environ);
+		
+		if (val == -1)
+		{
+			perror("Error executing command");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		int status;
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+		{
+			printf("Command executed successfully with exit status: %d\n", WEXITSTATUS(status));
+		}
+		else
+		{
+			printf("Command execution failed\n");
+		}
+	}
+	free(non_const_command);
+}
+
+/**
+* listExecutables - lists files in bin
+* return: nothing
+*/
+
+void listExecutables()
+{
+	DIR *dir = opendir("/bin");
+	struct dirent *entry;
+	struct stat file_stat;
+
+	if (!dir)
+	{
+		perror("Error opening directory");
+        return;
+	}
+	while ((entry = readdir(dir)) != NULL)
+	{
+		size_t path_len = strlen("/bin/") + strlen(entry->d_name) + 1;
+		char *file_path = (char *)malloc(path_len);
+		if (file_path != NULL)
+		{
+			snprintf(file_path, path_len, "/bin/%s", entry->d_name);
+			if (stat(file_path, &file_stat) == -1)
+			{
+				perror("Error checking file status");
+				continue;
+			}
+			if (S_ISREG(file_stat.st_mode) && (file_stat.st_mode & S_IXUSR))
+			{
+				executeCommand(file_path);
+			}
+		}
+	}
+	closedir(dir);
+}
+
+
+
+
+
 /**
 * main - prints a prompt for the user
 * @ac: argument count
@@ -9,14 +103,9 @@
 
 int main()
 {
-	char *last_slash;
-	char *command = NULL;
+	char *xit = "exit", *command = NULL;
 	size_t n = 0;
-	pid_t pid;
-	int val;
 	int len;
-	char *xit = "exit";
-	struct stat file_stat;
 
 	do	
 	{
@@ -41,8 +130,14 @@ int main()
 		command[len - 1] = '\0';
         }
 	if (strcmp(command, xit) == 0)
+		break;
+	if (strcmp(command, "/bin/lsbin") == 0)
+	{
+		listExecutables();
 		continue;
-	if (stat(command, &file_stat) == -1)
+	}
+	executeCommand(command);
+	/**if (stat(command, &file_stat) == -1)
 	{
 		perror("Error checking executable");
 		continue;
@@ -55,7 +150,9 @@ int main()
 	pid = fork();
         if (pid == -1)
 	{
-
+		perror("Fork failed");
+		free(command);
+		command = NULL;
 		continue;
 	}
 	else if (pid == 0)
@@ -91,7 +188,9 @@ int main()
 			printf("Command execution failed\n");
 			continue;
 		}
-	}
+	}*/
+
 	}while (strcmp(command, xit) != 0);
+	free (command);
 	return 0;
 }
